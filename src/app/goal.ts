9a235d37a62b0f336d28f53f14bc3ae224386b5e
Santa claus <santa@nortpole.com>
2021-05-13 14:00:46 +0300

    add all content

---
+++ src/app/goal.ts
@@ -0,0 +1,18 @@
+// export class goals {
+//     id: number;
+//     name: string;
+// }
+
+// export class Goal {
+//   showDescription: boolean;
+//   constructor(public id: number,public name: string,public description: string){
+//     this.showDescription=false;
+//   }
+// }
+
+export class Goal {
+  public showDescription: boolean;
+  constructor(public id: number,public name: string,public description: string, public completeDate: Date){
+    this.showDescription=false;
+  }
+}
